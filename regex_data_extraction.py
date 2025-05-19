465e93c0e9da80868dff6be68a6adfa8c5924077
Monica Dhieu <m.dhieu@alustudent.com>
2025-05-19 20:37:43 +0200

    Add execute permission to the file for the user.

--- regex_data_extraction.py
+++ regex_data_extraction.py
@@ -1,1 +1,9 @@
+import os
+import stat
+import sys
 
+# Add execute permission to the file for the user.
+if __name__ == "__main__":
+    script_path = os.path.abspath(sys.argv[0])
+    current_permissions = os.stat(script_path).st_mode
+    os.chmod(script_path, current_permissions | stat.S_IXUSR)
